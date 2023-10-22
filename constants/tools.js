32357c265844ead87bb0102799ae1db9e14e9a0f
aeschyllus <lapadamarkanthony@gmail.com>
2023-10-22 23:24:04 +0800

    Update tools used

--- constants/tools.js
+++ constants/tools.js
@@ -1,31 +1,19 @@
-import {
-  FaCss3Alt,
-  FaGitAlt,
-  FaLaravel,
-} from "react-icons/fa";
+import { FaLaravel } from "react-icons/fa";
 import { GrReactjs } from "react-icons/gr";
 import {
-  SiDjango,
-  SiHtml5,
-  SiJavascript,
-  SiNeovim,
+  SiNestjs,
   SiNextdotjs,
+  SiPrisma,
   SiRedux,
-  SiReduxsaga,
   SiTailwindcss,
 } from "react-icons/si";
 
 export const TOOLS = [
-  { id: 1, title: "CSS", icon: <FaCss3Alt /> },
-  { id: 2, title: "Django", icon: <SiDjango /> },
-  { id: 3, title: "Git", icon: <FaGitAlt /> },
-  { id: 4, title: "HTML", icon: <SiHtml5 /> },
-  { id: 5, title: "Javascript", icon: <SiJavascript /> },
-  { id: 6, title: "Laravel", icon: <FaLaravel /> },
-  { id: 7, title: "Neovim", icon: <SiNeovim /> },
-  { id: 8, title: "Next.js", icon: <SiNextdotjs /> },
-  { id: 9, title: "React", icon: <GrReactjs /> },
-  { id: 10, title: "Redux-Saga", icon: <SiReduxsaga /> },
-  { id: 11, title: "Redux", icon: <SiRedux /> },
-  { id: 12, title: "Tailwind", icon: <SiTailwindcss /> },
+  { id: 1, title: "React", icon: <GrReactjs /> },
+  { id: 2, title: "Next.js", icon: <SiNextdotjs /> },
+  { id: 3, title: "NestJS", icon: <SiNestjs /> },
+  { id: 4, title: "Prisma", icon: <SiPrisma /> },
+  { id: 5, title: "Redux", icon: <SiRedux /> },
+  { id: 6, title: "Tailwind", icon: <SiTailwindcss /> },
+  { id: 7, title: "Laravel", icon: <FaLaravel /> },
 ];
