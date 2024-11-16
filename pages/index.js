2f5178ce69b740a6496e9b672a58e611e755b692
aeschyllus <lapadamarkanthony@gmail.com>
2024-11-17 07:56:26 +0800

    chore: update portfolio
    - update role (fullstack -> frontend)
    - update about section
    - removed WIP from xpense api

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
     },
     {
       id: 2,
-      title: "Xpense API (WIP)",
+      title: "Xpense API",
       description: "Expense tracker",
       link: "https://github.com/aeschyllus/expensetracker-be",
       icon: <GrMoney className="text-5xl" />,
@@ -78,7 +78,7 @@ export default function Home() {
         <div>
           <p>Hi, my name is</p>
           <h1 className="font-bold text-3xl">Mark Anthony Lapada</h1>
-          <p className="text-right">Fullstack Developer</p>
+          <p className="text-right">Frontend Developer</p>
           <div className="flex items-center justify-end space-x-4 mt-3">
             {PROFILE_LINKS.map((profileLink) => (
               <a
@@ -97,17 +97,40 @@ export default function Home() {
         <Spacer />
         <h2 className="text-2xl font-bold tracking-widest">/about</h2>
         <p>
-          I&apos;m a Fullstack Developer, based in the Philippines, specializing
-          in building exceptionally digital experiences. Currently I&apos;m
-          focused on building responsive frontend web applications.
+          Hey there! I&apos;m Mark, your friendly neighborhood frontend
+          developer and all-around code aficionado. Picture me with a cup of
+          coffee in one hand, a keyboard under my fingertips, and a mischievous
+          grin on my face.
         </p>
+
         <p>
-          I graduated with a degree in Bachelor of Science in Information System
-          in the Technological University of the Philippines in 2019.
+          I live for the thrill of crafting stunning user interfaces that make
+          websites pop and dance like nobody&apos;s watching. From pixel-perfect
+          designs to seamless animations, I&apos;ve got a knack for turning code
+          into art. But don&apos;t let my serious coding face fool you—I&apos;ve
+          got a wicked sense of humor that I can&apos;t help but infuse into my
+          work.
         </p>
+
         <p>
-          Oh, I use Neovim as my code editor by the way. I recently transitioned
-          from using VS Code and I&apos;m never going back.
+          But my skills go beyond just coding. I have a secret power: the
+          ability to read clients&apos; minds and transform their vague ideas
+          into breathtaking websites. It&apos;s like I have an HTML-to-English
+          dictionary stored in my brain.
+        </p>
+
+        <p>
+          So, if you&apos;re looking for a frontend developer who can bring your
+          wildest web dreams to life while keeping you entertained with my
+          quirky sense of humor, look no further. Just be prepared for a few
+          (okay, maybe more than a few) puns along the way—because laughter is
+          the best syntax sugar, right?
+        </p>
+
+        <p>
+          I can&apos;t wait to embark on this coding adventure with you.
+          Let&apos;s make the web a more beautiful and functional place, one
+          line of code at a time!
         </p>
       </section>
 
